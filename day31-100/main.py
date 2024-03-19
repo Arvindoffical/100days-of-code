49cb25330a19032352eeca529d155e8b331d6264
Arvind Garg <109409974+Arvindoffical@users.noreply.github.com>
2024-03-19 20:10:18 +0530

    31
    flash card

--- day31-100/main.py
+++ day31-100/main.py
@@ -1,16 +1,14 @@
+import random
 from tkinter import *
 import pandas
-import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
-
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_csv("words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("data/french_words.csv")
-    print(original_data)
+    original_data = pandas.read_csv("transcript.csv")
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
@@ -34,23 +32,25 @@ def flip_card():
 
 def is_known():
     to_learn.remove(current_card)
-    print(len(to_learn))
     data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    data.to_csv("words_to_lean.csv", index=False)
     next_card()
 
 
+
+
+
 window = Tk()
-window.title("Flashy")
+window.title("flash card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
+card_front_img = PhotoImage(file='images/card_front.png')
+card_back_img = PhotoImage(file='images/card_back.png')
 card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(400, 150, text="", font=("ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -66,6 +66,3 @@ known_button.grid(row=1, column=1)
 next_card()
 
 window.mainloop()
-
-
-
